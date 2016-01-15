b99ae397926e1fa945f698dfa3781c8f81c94b75
Theofilos Intzoglou <int.teo@gmail.com>
2016-01-15 23:15:05 +0200

    Give the popup some style

--- popup.cpp
+++ popup.cpp
@@ -18,6 +18,9 @@ Popup::Popup(QWidget *parent) :
                 );
 
     connect(ui->button, SIGNAL(clicked()), this, SLOT(closePopup()));
+    //setStyleSheet(QString("QDialog {background-color: yellow}"));
+    ui->title->setStyleSheet(QString("QLabel {background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #E0E0E0, stop: 1 #000000);}"));
+    //ui->description->setStyleSheet(QString("QLabel {background-color: rgba(0,0,0,25%);}"));
 }
 
 Popup::~Popup()
