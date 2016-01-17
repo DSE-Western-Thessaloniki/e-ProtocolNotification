2e4b46760004ad64f677aa3527675f87d2686440
Theofilos Intzoglou <int.teo@gmail.com>
2016-01-17 12:40:54 +0200

    * FileDownloader checks for successfully completed downloads and does hash checking to check for file integrity

--- filedownloader.cpp
+++ filedownloader.cpp
@@ -4,6 +4,7 @@ FileDownloader::FileDownloader(QObject *parent) :
  QObject(parent)
 {
     connect(&netManager, SIGNAL (finished(QNetworkReply*)), this, SLOT (fileDownloaded(QNetworkReply*)));
+    connect(this, SIGNAL(downloaded()), this, SLOT(checkHash()));
 }
 
 FileDownloader::~FileDownloader() { }
@@ -16,7 +17,9 @@ void FileDownloader::fileDownloaded(QNetworkReply* reply) {
         downloadedDataMap[key] = reply->readAll();
     }
     else {
+        qDebug() << "Error Downloading " << reply->request().url();
         qDebug() << "Download Error: " << reply->errorString();
+        err = DownloadFailed;
     }
     reply->deleteLater();
     remainingRequests--;
@@ -31,13 +34,28 @@ QMap<int, QByteArray> FileDownloader::downloadedData() const {
 void FileDownloader::getFiles(QVariantList filelist, QUrl baseUrl)
 {
     if (!remainingRequests) { // Accept new list only when the previous is done
+        // Init lists
+        err = NoError;
+        downloadedDataMap.clear();
+        filenameList.clear();
+        hashList.clear();
+        hashOK.clear();
+        hashTypeList.clear();
+        urlList.clear();
+
         int size = filelist.size();
         if ((size % 3) == 0) {
             remainingRequests = size / 3;
             for (int i=0; i<size; i+=3) {
                 filenameList << filelist[0].toString();
                 hashList << filelist[1].toString();
-                hashTypeList << filelist[2].toInt();
+                QString hash = filelist[2].toString();
+                if (hash == "md5") {
+                    hashTypeList << QCryptographicHash::Md5;
+                }
+                else if (hash == "sha1") {
+                    hashTypeList << QCryptographicHash::Sha1;
+                }
                 urlList << QUrl(baseUrl.toString() + filelist[0].toString());
                 netManager.get(QNetworkRequest(QUrl(baseUrl.toString() + filelist[0].toString())));
             }
@@ -47,4 +65,24 @@ void FileDownloader::getFiles(QVariantList filelist, QUrl baseUrl)
         }
     }
     qDebug() << "Download error: Downloads still pending. Not queuing.";
+}
+
+void FileDownloader::checkHash()
+{
+    if (err == NoError) { // If all files were downloaded without problems
+        int lsize = filenameList.size();
+
+        for (int i=0; i<lsize; i++) {
+            QByteArray tmpHash;
+            tmpHash = QCryptographicHash::hash(downloadedDataMap[i],hashTypeList[i]);
+            if (tmpHash == hashList[i]) {
+                hashOK << true;
+            }
+            else {
+                hashOK << false;
+                err = HashCheckFailed;
+            }
+        }
+    }
+    emit hashChecked();
 }
--- filedownloader.h
+++ filedownloader.h
@@ -14,14 +14,20 @@ class FileDownloader : public QObject
  public:
   explicit FileDownloader(QObject *parent = 0);
   virtual ~FileDownloader();
-  QMap<int, QByteArray> downloadedData() const;
+  QMap<int, QByteArray> downloadedData(void) const;
   void getFiles(QVariantList filelist, QUrl baseUrl);
+  Error error(void);
+
+public:
+  enum Error {NoError, DownloadFailed, HashCheckFailed};
 
  signals:
   void downloaded();
+  void hashChecked();
 
  private slots:
   void fileDownloaded(QNetworkReply* reply);
+  void checkHash();
 
  private:
   QString hash;
@@ -30,9 +36,11 @@ class FileDownloader : public QObject
   QMap<int,QByteArray> downloadedDataMap;
   QStringList filenameList;
   QStringList hashList;
-  QList<int> hashTypeList;
+  QList<bool> hashOK;
+  QList<QCryptographicHash::Algorithm> hashTypeList;
   QList<QUrl> urlList;
   int remainingRequests;
+  Error err;
 };
 
 #endif // FILEDOWNLOADER_H
